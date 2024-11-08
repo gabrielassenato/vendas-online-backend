f0e4089a7fb9717db1077864d96ead5b8369ba6a
Gabriel Assenato <gabrielassenato@gmail.com>
2024-11-08 20:49:05 -0300

    :sparkles: feat: create DTO using pipe

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,20 @@
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  ValidationPipe,
+  UsePipes,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create.user.dto';
 import { UserService } from './user.service';
 import { UserEntity } from './entities/user.entity';
+import { ReturnUserDto } from './dtos/returnUser.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+  @UsePipes(ValidationPipe)
   @Post()
   //função aync diz que enquanto não tiver tudo feito ela não vai terminar
   async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
@@ -13,7 +22,9 @@ export class UserController {
   }
 
   @Get()
-  async getAllUser(): Promise<UserEntity[]> {
-    return this.userService.getAllUser();
+  async getAllUser(): Promise<ReturnUserDto[]> {
+    return (await this.userService.getAllUser()).map(
+      (userEntity) => new ReturnUserDto(userEntity),
+    );
   }
 }
